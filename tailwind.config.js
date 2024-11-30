79d16c1cfb36db2701e439b2c5a76b7181f23267
SANJOY <paulsanjoy2923@gmail.com>
2024-11-30 12:43:08 +0530

    chill fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        fontChillOne: [ "Rampart One" ],
+        fontChillTwo: [ "Indie Flower" ]
+      }
+    },
   },
   plugins: [],
 }
