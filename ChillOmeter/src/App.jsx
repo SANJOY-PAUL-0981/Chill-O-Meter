e08eb44f4daabc7ea44a73cd2fac77dcce7da71b
SANJOY <paulsanjoy2923@gmail.com>
2024-12-03 21:47:01 +0530

    gemini api added

--- ChillOmeter/src/App.jsx
+++ ChillOmeter/src/App.jsx
@@ -3,13 +3,42 @@ import "./App.css";
 import chillGuy from "../src/assets/chillGuy.jpg";
 import NavBar from "./ui/NavBar";
 import Footer from "./ui/Footer";
-import { fetchUserID } from "./fetchUserID"; 
-import { fetchTweets } from "./fetchTweets"; 
+import { fetchUserID } from "./fetchUserID";
+import { fetchTweets } from "./fetchTweets";
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from '@google/generative-ai';
 
 function App() {
   const [username, setUsername] = useState("");
+  const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+  const genAI = new GoogleGenerativeAI(API_KEY);
 
-  {/*fetch user ID and tweets*/}
+  {/*safety setting initilization*/ }
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+  ];
+
+  {/*model initilization*/ }
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    safetySettings: safetySettings,
+  });
+
+  {/*fetch user ID and tweets*/ }
   const handleFetchData = async () => {
     if (!username) {
       alert("Please enter a username!");
@@ -17,17 +46,39 @@ function App() {
     }
 
     try {
-      {/*Fetch User ID*/}
+      {/*Fetch User ID*/ }
       const userId = await fetchUserID(username);
       console.log("Fetched User ID:", userId);
 
-      {/*Fetch Tweets */}
+      {/*Fetch Tweets */ }
       const tweetsList = await fetchTweets(userId);
-      console.log(tweetsList); 
+      console.log(tweetsList);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
+
+  const prompt = `Rate the user's Chill nature and tell the percentage of his chill, based on their tweets,
+  Rules:
+  1. DO NOT EXPLAIN ANYTHING ONLY TELL THE VALUE.
+  2. NO NEED TO EXPLAIN THE REASONS BEHIND VALUE. 
+  3. JUJST ONLY GENERATE THE VALUE.
+  4. GENERATE THE ANSWER BEING 200% SURE ON PERCENTAGE VALUE.
+  5. THE ANSWER WILL BE IN THIS FORMAT " You {RESULT}% chill".
+  6. THE CHILL RATE CAN BE ANY NUMBER BETWEEN 0-100.
+  Here are the Tweets: 
+  It's been a long time since I have seen any new anime 😔
+  Bruh😔....
+Whatever nnn is over
+Bro is Batman now 🦇
+next year I will try to make if full green`;
+
+  async function generateChillScore() {
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+  }
+
+  generateChillScore();
 
   return (
     <>
@@ -69,12 +120,12 @@ function App() {
             id="input"
             type="text"
             value={username}
-            onChange={(e) => setUsername(e.target.value)} 
+            onChange={(e) => setUsername(e.target.value)}
             placeholder="Enter Twitter username"
             className="border-black/70 focus:outline-none focus:border-2 focus:shadow-[-10px_-10px_30px_4px_rgba(171,130,97,0.1),_10px_10px_30px_4px_rgba(171,130,97,0.15)] transition-all duration-300 focus:border-[#d8b69a] border-[1.55px] h-20 w-[40vw] p-5 text-xl font-fontChillTwo"
           />
           <button
-            onClick={handleFetchData} 
+            onClick={handleFetchData}
             className="font-fontChillOne h-20 w-40 hover:shadow-[-10px_-10px_30px_4px_rgba(171,130,97,0.1),_10px_10px_30px_4px_rgba(171,130,97,0.15)] transition-all duration-200 border-4 rounded text-2xl text-center border-[#ab8261] bg-[#ffffff]"
           >
             Let's Chill
