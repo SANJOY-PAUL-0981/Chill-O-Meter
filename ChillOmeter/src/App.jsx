454d1941199d558bad4127b40e06bebd5783c4d3
SANJOY <paulsanjoy2923@gmail.com>
2024-12-04 20:48:30 +0530

    responsive done

--- ChillOmeter/src/App.jsx
+++ ChillOmeter/src/App.jsx
@@ -64,42 +64,44 @@ function App() {
   return (
     <>
       <NavBar />
-      <div className="flex flex-col justify-center items-center h-[90vh] mb-20">
-        <div className="border-2 border-amber-800/20 w-[80vw] h-[80vh] rounded-3xl flex justify-between items-center p-14 gap-10 shadow-[-10px_-10px_30px_4px_rgba(171,130,97,0.1),_10px_10px_30px_4px_rgba(171,130,97,0.45)]">
+      <div className="flex flex-col justify-center items-center h-[90vh] mb-20 md:mb-10">
+        <div className="border-2 border-amber-800/20 w-[90vw] md:w-[80vw] xl:h-[80vh] h-[100vh] md:h-auto rounded-3xl flex flex-col md:flex-row justify-between items-center xl:p-8 p-8 xl:px-10 md:p-14 xl:gap-20 shadow-[-10px_-10px_30px_4px_rgba(171,130,97,0.1),_10px_10px_30px_4px_rgba(171,130,97,0.45)]">
+
           <div>
-            <div className="w-[40vw] flex flex-col gap-5">
-              <h1 className="text-7xl font-fontChillOne">Are You Just A Chill Guy?</h1>
-              <p className="font-fontChillTwo text-2xl font-semibold text-[#ab8261]">
+            <div className="flex flex-col gap-5 text-center md:text-left">
+              <h1 className="text-5xl md:text-7xl font-fontChillOne">Are You Just A Chill Guy?</h1>
+              <p className="font-fontChillTwo text-lg md:text-2xl font-semibold text-[#ab8261]">
                 <span>How Chill Are You?</span> Let’s Measure Your Chillness with the help of Chill O Meter!
               </p>
             </div>
             <a
               href="#input"
-              className="font-fontChillTwo text-xl text-blue-500 font-semibold underline"
+              className="font-fontChillTwo text-md md:text-xl text-blue-500 font-semibold underline"
             >
               Click Here <span className="font-fontChillOne">!</span>
             </a>
           </div>
 
-          <div>
-            <img src={chillGuy} alt="chillguy" className="size-96 rounded-3xl" />
+          <div className="xl:w-[35vw]">
+            <img src={chillGuy} alt="chillguy" className="xl:size-96 size-80 rounded-3xl" />
           </div>
+
         </div>
       </div>
 
-      <div className="h-[40vh] flex justify-center items-center">
-        <div className="flex items-center gap-5">
+      <div className="h-auto md:h-[40vh] flex flex-col justify-center items-center gap-5">
+        <div className="flex flex-col md:flex-row items-center gap-5">
           <input
             id="input"
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             placeholder="Enter Twitter username"
-            className="border-black/70 focus:outline-none focus:border-2 focus:shadow-[-10px_-10px_30px_4px_rgba(171,130,97,0.1),_10px_10px_30px_4px_rgba(171,130,97,0.15)] transition-all duration-300 focus:border-[#d8b69a] border-[1.55px] h-20 w-[40vw] p-5 text-xl font-fontChillTwo"
+            className="border-black/70 focus:outline-none focus:border-2 focus:shadow-[-10px_-10px_30px_4px_rgba(171,130,97,0.1),_10px_10px_30px_4px_rgba(171,130,97,0.15)] transition-all duration-300 focus:border-[#d8b69a] border-[1.55px] h-16 md:h-20 w-[90vw] md:w-[40vw] p-5 text-lg md:text-xl font-fontChillTwo"
           />
           <button
             onClick={handleFetchData}
-            className="font-fontChillOne h-20 w-40 hover:shadow-[-10px_-10px_30px_4px_rgba(171,130,97,0.1),_10px_10px_30px_4px_rgba(171,130,97,0.15)] transition-all duration-200 border-4 rounded text-2xl text-center border-[#ab8261] bg-[#ffffff]"
+            className="font-fontChillOne h-16 md:h-20 w-[30vw] md:w-40 hover:shadow-[-10px_-10px_30px_4px_rgba(171,130,97,0.1),_10px_10px_30px_4px_rgba(171,130,97,0.15)] transition-all duration-200 border-4 rounded text-lg md:text-2xl text-center border-[#ab8261] bg-[#ffffff]"
           >
             Let's Chill
           </button>
@@ -108,13 +110,13 @@ function App() {
 
       {loading ? (
         <div className="text-center p-10 flex justify-center items-center">
-          <img src="../src/assets/loading.gif" alt="Loading..." className="loader size-40" />
+          <img src="../src/assets/loading.gif" alt="Loading..." className="loader w-20 md:w-40" />
         </div>
       ) : (
         chillScore && (
-          <div className="text-center text-lg p-10 flex justify-center items-center">
-            <div className="w-[45vw]">
-              <h2 className="text-3xl font-fontChillOne">{chillScore}</h2>
+          <div className="text-center text-sm md:text-lg p-10 flex justify-center items-center">
+            <div className="w-[90vw] md:w-[45vw]">
+              <h2 className="text-2xl md:text-3xl font-fontChillOne">{chillScore}</h2>
             </div>
           </div>
         )
--- ChillOmeter/src/ui/NavBar.jsx
+++ ChillOmeter/src/ui/NavBar.jsx
@@ -1,15 +1,23 @@
-function NavBar(){
-    return(
-        <div className="pt-5 pl-10 pb-2 flex justify-between">
-            <p className="font-fontChillTwo font-bold text-3xl">
+function NavBar() {
+    return (
+        <div className="pt-5 px-4 xl:px-20 xl:pb-2 pb-10 flex justify-between items-center sm:items-start">
+            <p className="font-fontChillTwo font-bold text-xl sm:text-3xl text-center sm:text-left">
                 Chill <span className="font-fontChillOne font-light text-[#ab8261]">O</span> Meter
             </p>
-            <ul className="flex gap-10 px-20">
-                <li className="text-xl text-yellow-600 cursor-pointer font-semibold font-fontChillOne"><a target="_blank" href="https://x.com/Sanj0yX">X</a></li>
-                <li className="text-xl text-yellow-600 cursor-pointer font-semibold font-fontChillOne"><a target="_blank" href="https://sanjoypaul.vercel.app/">Portfolio</a></li>
+            <ul className="flex sm:flex-row sm:gap-10 gap-2 justify-center items-center">
+                <li className="text-base sm:text-xl text-yellow-600 cursor-pointer font-semibold font-fontChillOne">
+                    <a target="_blank" href="https://x.com/Sanj0yX" rel="noopener noreferrer">
+                        X
+                    </a>
+                </li>
+                <li className="text-base sm:text-xl text-yellow-600 cursor-pointer font-semibold font-fontChillOne">
+                    <a target="_blank" href="https://sanjoypaul.vercel.app/" rel="noopener noreferrer">
+                        Portfolio
+                    </a>
+                </li>
             </ul>
         </div>
-    )
+    );
 }
 
 export default NavBar;
